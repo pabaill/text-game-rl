d0db0778ee946a49a49a3ab5895ef3821c0d7f9c
pabaill <pabaill@stanford.edu>
2025-05-20 23:11:31 +0000

    Add timeout for data generation

--- data/generate.py
+++ data/generate.py
@@ -8,6 +8,8 @@ import os
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(parent_dir)
 
+import time
+
 EVAL = ["yomomma", "gold", "jewel", "lurking", "night"]
 TEST = ["zork1", "snacktime"]
 
@@ -26,6 +28,9 @@ def save_to_csv(data, filename="training_data.csv"):
 
 def generate_dataset(gamename, n_walkthroughs=5, p_rand=0.1):
     env = FrotzEnv(f"../jericho/z-machine-games-master/jericho-game-suite/{gamename}")
+
+    # Time out after 20 minutes
+    timeout = time.time() + (60 * 20)
 
     for i in tqdm(range(n_walkthroughs)):
         prev_observation, info = env.reset()
@@ -49,8 +54,8 @@ def generate_dataset(gamename, n_walkthroughs=5, p_rand=0.1):
                 env.set_state(prev_state)
                 observation, reward, done, info = env.step(action)
             prev_observation = observation
-            # if done or len(data) >= 3000:
-            #     break
+            if done or time.time() > timeout:
+                break
         prev_observation, info = env.reset()
     folder = "train"
     if gamename[:-3] in EVAL:
@@ -63,7 +68,7 @@ if __name__ == '__main__':
     gamelist = os.listdir('../jericho/z-machine-games-master/jericho-game-suite')
     p_rand = float(input("p_rand: "))
     existing_games = os.listdir('train')
-    games_to_skip = ["anchor.z8", "ballyhoo.z3"]
+    games_to_skip = ["905.z5", "acorncourt.z5", "advent.z5", "adventureland.z5", "afflicted.z8", "awaken.z5", "balances.z5", "anchor.z8", "ballyhoo.z3"]
     for gamename in gamelist:
         if gamename not in games_to_skip and f"training_data_{gamename}_p_rand_{p_rand}.csv" not in existing_games:
             print(f"Generating data for : {gamename}")
