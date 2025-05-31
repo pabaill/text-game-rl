729bb266a9c89a97b00ada3143abc49ab56fa36b
pabaill <pabaill@stanford.edu>
2025-05-31 16:24:29 -0700

    Eval w/ quant results

--- SAC/online_eval.py
+++ SAC/online_eval.py
@@ -13,16 +13,19 @@ def eval(game_path, actor_ckpt_path, state_dim=3072, action_dim=3072, max_ep_len
     actor = Actor(state_dim=state_dim, action_dim=action_dim)
     actor.load_state_dict(torch.load(actor_ckpt_path, map_location=torch.device('cpu')))
     actor.eval()
-    
-    valid_actions = env.get_valid_actions()
-    embedding_to_action = generate_embedding_action_dict(valid_actions, llama)
 
     state_text, info = env.game.reset()
+    direction_count, meta_count, interaction_count = 0, 0, 0
+    game_dict = env.get_dictionary()
+    nav_words = [item.word for item in game_dict if item.is_dir]
+    meta_words = [item.word for item in game_dict if item.is_meta]
 
     results = []
     done = False
     ep_rem = max_ep_len
     while not done and ep_rem > 0:
+        valid_actions = env.get_valid_actions()
+        embedding_to_action = generate_embedding_action_dict(valid_actions, llama)
         state_embedding = llama.encode_text(state_text)
         action_embedding = actor(state_embedding)
         action_text = decode_action(action_embedding, embedding_to_action, is_training=False)
@@ -36,11 +39,29 @@ def eval(game_path, actor_ckpt_path, state_dim=3072, action_dim=3072, max_ep_len
             "current_score": info['score']
         })
 
+        if action_text in nav_words:
+            direction_count += 1
+        if action_text in meta_words:
+            meta_count += 1
+        else:
+            interaction_count += 1
+
         state_text = next_state_text
         ep_rem -= 1
 
     results_df = pd.DataFrame(results)
     results_df.to_csv(output_file, index=False)
+
+    # Summary Stats
+    total_reward = sum([result['reward'] for result in results])
+    avg_reward = total_reward / len(results)
+    print(f"Average Reward: {avg_reward}")
+    final_score = info['score']
+    print(f"Final Score: {final_score}")
+    total_actions = direction_count + meta_count + interaction_count
+    episode_length = max_ep_len - ep_rem
+    print(f"Episode Length: {episode_length}")
+    print(f"Total actions: {total_actions} | Navigation: {direction_count / total_actions} | Meta: {meta_count / total_actions} | Interaction: {interaction_count / total_actions}")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Train RL agent from CSV dataset.")
